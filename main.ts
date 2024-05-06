867b2f702fcb639e6cf483a3e6d5e00a27e1bc64
reididl <78746165+reididl@users.noreply.github.com>
2024-05-06 20:02:37 +0200

    Affiche distance et angle sur OLED,buzzer, led clignote

--- main.ts
+++ main.ts
@@ -1,109 +1,112 @@
 function RADAR165TO15 () {
     i = 165
     while (i > 15) {
-        BOUTONOFF = pins.digitalReadPin(DigitalPin.P2)
+        pins.digitalWritePin(DigitalPin.P12, 1)
+        pins.analogWritePin(AnalogPin.P0, 400)
         robotbit.GeekServo5KG(robotbit.Servos.S1, i)
+        OLED12864_I2C.clear()
         OLED12864_I2C.showString(
         0,
         0,
         "D:",
         1
         )
-        OLED12864_I2C.showNumber(
-        0,
-        0,
-        sonar.ping(
+        distance = sonar.ping(
         DigitalPin.P15,
         DigitalPin.P14,
         PingUnit.Centimeters
-        ),
+        )
+        control.waitMicros(2)
+        OLED12864_I2C.showNumber(
+        3,
+        0,
+        distance,
+        1
+        )
+        OLED12864_I2C.showString(
+        8,
+        0,
+        "cm",
         1
         )
         OLED12864_I2C.showString(
         0,
-        0,
-        " cm",
-        1
-        )
-        OLED12864_I2C.showString(
-        20,
-        0,
+        1,
         "A:",
         1
         )
         OLED12864_I2C.showNumber(
-        0,
-        0,
+        3,
+        1,
         i,
         1
         )
-        OLED12864_I2C.showString(
-        0,
-        0,
-        "°",
-        1
-        )
+        basic.pause(30)
+        pins.digitalWritePin(DigitalPin.P12, 0)
+        pins.analogWritePin(AnalogPin.P0, 0)
         basic.pause(30)
         i = i - 1
     }
 }
 function RADAR15TO165 () {
     for (let j = 0; j <= 150; j++) {
-        BOUTONOFF = pins.digitalReadPin(DigitalPin.P2)
+        pins.digitalWritePin(DigitalPin.P12, 1)
+        pins.analogWritePin(AnalogPin.P0, 600)
         robotbit.GeekServo5KG(robotbit.Servos.S1, CONSTANGLE + j)
+        OLED12864_I2C.clear()
         OLED12864_I2C.showString(
         0,
         0,
         "D:",
         1
         )
-        OLED12864_I2C.showNumber(
-        0,
-        0,
-        sonar.ping(
+        distance = sonar.ping(
         DigitalPin.P15,
         DigitalPin.P14,
         PingUnit.Centimeters
-        ),
+        )
+        control.waitMicros(2)
+        OLED12864_I2C.showNumber(
+        3,
+        0,
+        distance,
+        1
+        )
+        OLED12864_I2C.showString(
+        8,
+        0,
+        "cm",
         1
         )
         OLED12864_I2C.showString(
         0,
-        0,
-        " cm",
-        1
-        )
-        OLED12864_I2C.showString(
-        20,
-        0,
+        1,
         "A:",
         1
         )
         OLED12864_I2C.showNumber(
-        0,
-        0,
+        3,
+        1,
         CONSTANGLE + j,
         1
         )
-        OLED12864_I2C.showString(
-        0,
-        0,
-        "°",
-        1
-        )
-        basic.pause(30)
+        basic.pause(100)
+        pins.digitalWritePin(DigitalPin.P12, 0)
+        pins.analogWritePin(AnalogPin.P0, 0)
+        basic.pause(100)
     }
 }
 let OFF = 0
 let ON = 0
-let BOUTONON = 0
 let BOUTONOFF = 0
+let BOUTONON = 0
+let distance = 0
 let i = 0
 let CONSTANGLE = 0
 CONSTANGLE = 15
 pins.digitalWritePin(DigitalPin.P8, 0)
 pins.digitalWritePin(DigitalPin.P12, 0)
-pins.digitalWritePin(DigitalPin.P13, 0)
+pins.digitalWritePin(DigitalPin.P13, 1)
 robotbit.GeekServo5KG(robotbit.Servos.S1, 90)
 basic.showLeds(`
     # # # # #
@@ -112,9 +115,8 @@ basic.showLeds(`
     # . . . #
     # # # # #
     `)
-OLED.init(128, 64)
 OLED12864_I2C.init(60)
-OLED12864_I2C.zoom(false)
+OLED12864_I2C.zoom(true)
 OLED12864_I2C.on()
 OLED12864_I2C.clear()
 OLED12864_I2C.showString(
@@ -123,13 +125,14 @@ OLED12864_I2C.showString(
 "WAITING MODE, push ON!",
 1
 )
+basic.pause(2000)
+OLED12864_I2C.clear()
 basic.forever(function () {
     BOUTONON = pins.digitalReadPin(DigitalPin.P1)
     BOUTONOFF = pins.digitalReadPin(DigitalPin.P2)
     if (BOUTONON == 1) {
         ON = 1
         OFF = 0
-        OLED12864_I2C.clear()
     }
     if (BOUTONOFF == 1) {
         ON = 0
@@ -145,7 +148,7 @@ basic.forever(function () {
     if (OFF == 1) {
         pins.digitalWritePin(DigitalPin.P8, 0)
         pins.digitalWritePin(DigitalPin.P12, 0)
-        pins.digitalWritePin(DigitalPin.P13, 0)
+        pins.digitalWritePin(DigitalPin.P13, 1)
         music.stopAllSounds()
     }
 })
